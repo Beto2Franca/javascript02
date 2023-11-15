b0947e772d1a84422020c1fc1206e942658c886b
beto franca <beto2franca@gmail.com>
2023-11-14 21:39:49 -0300

    base JS

--- modulo01/console.js
+++ modulo01/console.js
@@ -1,1 +1,33 @@
-console.log('Hello, World!')
+// console.log('Hello, World!')
+
+// Storing Values with assignment operator
+ var a ;
+ var b = 2
+
+ a = 7
+ b = a
+
+ console.log(a)
+
+ // Initializing variables w/ assignment operator 
+
+ var a = 9
+
+ // Uninitialized Variables - Variáveis ​​não inicializadas
+// initialize these three variables 
+ var a = 5 
+ var b = 10
+ var c = "I am a"
+ // do not change code below this line
+ a = a+1
+ b = b+5
+ c=c+ "String"
+
+ //Case Sensitivity in Variables - Sensibilidade a maiúsculas e minúsculas em variáveis
+
+ // Declarations
+ var studlyCapVar
+ var properCamelCasa
+ //Assignments
+ studlyCapVar = 10
+ properCamlCase = "A String"
