4eb493da6cd46c023baf196e478a38e487c851fa
beto franca <beto2franca@gmail.com>
2023-11-30 17:41:12 -0300

    evento02

--- aulas-js/js/evento.js
+++ aulas-js/js/evento.js
@@ -2,3 +2,20 @@ function eventoClick(){
     alert('opa!')
     // document.body.style.backgroundColor = 'gray'
 }
+
+function viraVermelho(){
+    let div = document.getElementById("teste")
+    div.style.backgroundColor = 'red'
+}
+function viraAzul(){
+    let div = document.getElementById("teste")
+    div.style.backgroundColor = 'aqua'
+}
+function adicionarTexto(){
+    let p = document.getElementById("texto")
+    p.append('O mouse moveu')
+}
+function limpaTexto(){
+     document.getElementById("campoTexto").value = " "
+    //  campoTexto.style.backgroundColor = 'lightblue' onfocus
+}
