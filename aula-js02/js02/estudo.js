d813d378d6e96248f797ba69b8a8b46acdc7d81f
beto franca <beto2franca@gmail.com>
2023-12-04 14:56:56 -0300

    if e else

---
+++ aula-js02/js02/estudo.js
@@ -0,0 +1,18 @@
+// let lampada = 'off'
+
+// if(lampada == 'on'){
+//     alert('Lampada ligada')
+// }else{
+//     alert('lampada desligada')
+// }
+
+
+// let hora = new Date().getHours();
+
+// if(hora <12){
+//     alert('bom dia')
+// }else if(hora <18){
+//     alert('bora tarde')
+// }else{
+//     alert('boa noite')
+// }
