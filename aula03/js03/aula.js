e05c197373aa79b75b40e17a41334b50c7f8973e
beto franca <beto2franca@gmail.com>
2023-12-13 14:41:50 -0300

    mudança titulo

--- aula03/js03/aula.js
+++ aula03/js03/aula.js
@@ -13,9 +13,9 @@ for (let i =0; i < 101; i++){
 //     document.getElementById("ano").innerHTML +="<option value='"+i+"'>" + i + "</option>"
 // }
 
-let ano = new Date().getFullYear()
+// let ano = new Date().getFullYear()
 
-for(let i=ano; i >= 1900; i--){
-    document.getElementById("ano").innerHTML +="<option value='"+i+"'>" + i + "</option>"
-}
+// for(let i=ano; i >= 1900; i--){
+//     document.getElementById("ano").innerHTML +="<option value='"+i+"'>" + i + "</option>"
+// }
 
---
+++ aula05/js05/aula.js
@@ -0,0 +1,22 @@
+
+
+
+class Carro{
+    constructor(valor01, valor02, valor03){
+        this.marca = valor01
+        this.modelo = valor02
+        this.ano = valor03
+    }
+}
+
+const uno = new Carro("Fiat", "Uno", 2001)
+const gol = new Carro("Volkswagen", "Gol", 2013)
+console.log(uno)
+console.log(gol.ano)
+
+// const carro = {
+//     marca:"Fiat",
+//     Modelo: "Uno",
+//     Ano: 2001
+// }
+
